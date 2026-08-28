d3322c63df7ddbbf2d54884120a3ca69be2ca617
Reginaldo345 <juniorfb98@gmail.com>
2026-08-28 09:56:02 -0300

    commit inicial

---
+++ src/main/java/reginaldo/api_desafio_comunicacao/ApiDesafioComunicacaoApplication.java
@@ -0,0 +1,13 @@
+package reginaldo.api_desafio_comunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioComunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioComunicacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reginaldo/api_desafio_comunicacao/ApiDesafioComunicacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package reginaldo.api_desafio_comunicacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDesafioComunicacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
